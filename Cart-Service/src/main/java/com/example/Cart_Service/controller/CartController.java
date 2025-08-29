0ad4153210d81016158d84fc803399d1c0974613
prachi-katiyar16 <prachi.katiyar@ksolves.com>
2025-08-29 12:54:49 +0530

    Update CartController.java

--- Cart-Service/src/main/java/com/example/Cart_Service/controller/CartController.java
+++ Cart-Service/src/main/java/com/example/Cart_Service/controller/CartController.java
@@ -6,7 +6,6 @@ import com.example.Cart_Service.model.CartItemRemovalRequest;
 import com.example.Cart_Service.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
